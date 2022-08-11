26134f4e548669874453596791b0952dd1d32c16
Pau Civill <pau.civill@gmail.com>
2022-08-11 09:36:25 +0200

    post and post modals done

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-// let modalHtml = 
-// `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//     <div class="modal-dialog">
-//         <div class="modal-content">
-
-//             <div class="modal-header">
-//                 <h1 class="modal-title" id="exampleModalLabel"></h1>
-//                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//             </div>
-
-//             <div class="modal-body">
-//                 <p id="modal-post-body">Post body</p>
-//                 <div>
-//                     <h2 id="modal-user-title">User</h2>
-//                     <p id="modal-username">alex123</p>
-//                     <p id="modal-user-email"></p>alex@gmail.com</p>
-//                 </div>
-//             </div>
-
-//             <div class="modal-footer comments-section">
-//                 <h2>Comments</h2>
-//                 <button type="button" class="btn btn-primary">Load Comments</button>
-//             </div>
-
-//         </div>
-//     </div>
-// </div>`;
+let modalHtml;
 
 const apiUrl = "http://localhost:3000"
 const container = document.querySelector("section");
@@ -40,13 +14,13 @@ const modalUserEmail = document.querySelector("#modal-user-email");
 fetch(`${apiUrl}/posts`)
     .then(response => response.json())
     .then(posts => {
+        let num = 1;
         posts.forEach(post => {
-            // container.insertAdjacentHTML("afterbegin", modalHtml);
 
             let divPosts = document.createElement("div");
             divPosts.setAttribute("id", "div-posts");
             divPosts.setAttribute("data-bs-toggle", "modal");
-            divPosts.setAttribute("data-bs-target", "#exampleModal");
+            divPosts.setAttribute("data-bs-target", `#exampleModal${num}`);
 
             let pTitle = document.createElement("p");
             pTitle.setAttribute("class", "pTitle");
@@ -60,44 +34,36 @@ fetch(`${apiUrl}/posts`)
             pTitle.appendChild(document.createTextNode(`${post.title}`));
             pBody.appendChild(document.createTextNode(`${post.body}`));
 
-            // postDivs.addEventListener("click", () => {
-            //     modalDataPosts(post.title, post.body);
-            // });
+            modalHtml = 
+            `<div class="modal fade" id="exampleModal${num}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
 
-            // postDivs.forEach(div => {
-            //     div.addEventListener("click", modalDataPosts);
-            // })
-            // postDivs.addEventListener("click", modalDataPosts);
+                        <div class="modal-header">
+                            <h1 class="modal-title" id="exampleModalLabel">${post.title}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
 
-            // let modalH1Title = document.createElement("h1");
-            // modalHeader.appendChild(modalH1Title);
-            // modalH1Title.appendChild(document.createTextNode(post.title));
+                        <div class="modal-body">
+                            <p id="modal-post-body">${post.body}</p>
+                            <div>
+                                <h2 id="modal-user-title">User</h2>
+                                <p id="modal-username">alex123</p>
+                                <p id="modal-user-email"></p>alex@gmail.com</p>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer comments-section">
+                            <h2>Comments</h2>
+                            <button type="button" class="btn btn-primary">Load Comments</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>`;
+            divPosts.insertAdjacentHTML("afterbegin", modalHtml) ;
+
+            num++;
             
-            let modalPostTitle = document.querySelector("#exampleModalLabel");
-            modalPostTitle.appendChild(document.createTextNode(`${post.title}`));
         }); 
     });
-
-function modalDataPosts() {
-    console.log("holi")
-    // for (let i =1;i<postDivs.length;i++){
-    //     if(i===post.id){
-    //         // modalPostTitle.appendChild(document.createTextNode(`${post.title}`));
-
-    //         let modalH1Title = document.createElement("h1");
-    //         modalHeader.appendChild(modalH1Title);
-    //         modalH1Title.appendChild(document.createTextNode(`${post.title}`));
-    //         //pintame el h1 con post.id
-    //     }
-}
-
-
-    // modalPostTitle.appendChild(document.createTextNode(title));
-    // modalPostTitle.appendChild(document.createTextNode(body));
-
-    // let modalH1Title = document.createElement("h1");
-    // modalHeader.appendChild(modalH1Title)
-    // modalH1Title.appendChild(document.createTextNode(title));
-
-
-    
